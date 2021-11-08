ed4f3702ba432cfd3e9e3c2cb8aa735de2e034da
rosborne132 <ozborne132@gmail.com>
2021-11-07 21:12:57 -0800

    format code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -85,8 +85,9 @@
 			<button
 				type="submit"
 				class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-				>Start</button
 			>
+				Start
+			</button>
 		</form>
 	</section>
 </div>
