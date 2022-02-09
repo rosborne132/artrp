b199ce714fa853c4f92fc1f82041c1b54c1bc32e
ozborne132@gmail.com <ozborne132@gmail.com>
2022-02-08 19:50:44 -0800

    Add lambda unit tests

--- apps/skills-microservice/tests/functions/deleteSkills/hander.test.ts
+++ apps/skills-microservice/tests/functions/deleteSkills/hander.test.ts
@@ -1,15 +1,15 @@
-import { createSkills } from '../../../src/functions'
+import { deleteSkills } from '../../../src/functions'
 import { httpResponseCode, errorMsgMap, SkillsService } from '../../../src/libs'
 
-describe('createSkills tests', () => {
+describe('deleteSkills tests', () => {
   test('should return 200 with successful payload', async () => {
-    SkillsService.prototype.createSkills = jest.fn().mockResolvedValue({
+    SkillsService.prototype.deleteSkills = jest.fn().mockResolvedValue({
       data: 'Success payload!',
     })
 
-    const results = await createSkills({})
+    const results = await deleteSkills({})
     const expectedResults = {
-      statusCode: httpResponseCode.CREATED,
+      statusCode: httpResponseCode.SUCCESS,
       body: JSON.stringify({ data: 'Success payload!' }),
     }
 
@@ -18,11 +18,11 @@ describe('createSkills tests', () => {
 
   Object.entries(errorMsgMap).map((value) => {
     test(`should return ${value[1]} when error message '${value[0]}' is thrown`, async () => {
-      SkillsService.prototype.createSkills = jest
+      SkillsService.prototype.deleteSkills = jest
         .fn()
         .mockRejectedValue(new Error(value[0]))
 
-      const results = await createSkills({})
+      const results = await deleteSkills({})
       const expectedResults = { statusCode: value[1], body: value[0] }
 
       expect(results).toEqual(expectedResults)
